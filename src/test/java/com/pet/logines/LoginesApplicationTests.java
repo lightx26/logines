bfddb1592a346e4cbf52f265bf21c02d2fc71d86
lightx26 <ncnhatquang26@gmail.com>
2025-01-24 23:21:35 +0700

    First commit: Initialize project

---
+++ src/test/java/com/pet/logines/LoginesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pet.logines;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
